96492f561cf8067eabc7c8d5a37e79afa5c2315a
hodgestar <USER@DOMAIN>
2010-10-12 21:34:25 +0000

    0.6dev: Merged [924:925] from trunk.

--- bitten/upgrades.py
+++ bitten/upgrades.py
@@ -25,7 +25,7 @@ __docformat__ = 'restructuredtext en'
 
 # database abstraction functions
 
-def _parse_scheme(env):
+def parse_scheme(env):
     """Retrieve the environment database scheme."""
     connection_uri = DatabaseManager(env).connection_uri
     parts = connection_uri.split(':', 1)
@@ -36,7 +36,7 @@ def update_sequence(env, db, tbl, col):
     """Update a sequence associated with an autoincrement column."""
     # Hopefully Trac will eventually implement its own version
     # of this function.
-    scheme = _parse_scheme(env)
+    scheme = parse_scheme(env)
     if scheme == "postgres":
         seq = '%s_%s_seq' % (tbl, col)
         cursor = db.cursor()
@@ -47,7 +47,7 @@ def drop_index(env, db, tbl, idx):
     """Drop an index associated with a table."""
     # Hopefully Trac will eventually implement its own version
     # of this function.
-    scheme = _parse_scheme(env)
+    scheme = parse_scheme(env)
     cursor = db.cursor()
     if scheme == "mysql":
         cursor.execute("DROP INDEX %s ON %s" % (idx, tbl))
@@ -156,6 +156,8 @@ def add_last_activity_to_build(env, db):
                    "slave,started,stopped,last_activity,status) "
                    "SELECT id,config,rev,rev_time,platform,"
                    "slave,started,stopped,stopped,status FROM old_build_v11")
+
+    update_sequence(env, db, 'bitten_build', 'id')
 
 def add_config_to_reports(env, db):
     """Add the name of the build configuration as metadata to report documents
@@ -593,7 +595,7 @@ def add_config_platform_rev_index_to_build(env, db):
 
     if not duplicates_exist:
         cursor = db.cursor()
-        scheme = _parse_scheme(env)
+        scheme = parse_scheme(env)
         if scheme == "mysql":
             # 111 = 333 / len(columns in index) -- this is the Trac default
             cursor.execute("CREATE UNIQUE INDEX bitten_build_config_rev_platform_idx ON bitten_build (config(111), rev(111), platform)")
